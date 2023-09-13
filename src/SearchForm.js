89e19716c2f8577ab188b59770bf09e6ad090244
Jonathan Stern <jstern718@gmail.com>
2023-09-12 20:09:42 -0400

    breakpoints for search filter form

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -45,6 +45,16 @@ function SearchForm({ searchFunction, term }) {
 //     bubble.innerHTML = rangel.value;
 //   });
 
+{/* <label className="input-group-text bg-secondary p-2 col-3" htmlFor="inputGroup1">Min Employees</label>
+                <input type="range" className="form-range form-control p-2 col-9" min="0" max="500000" step = "500"
+                    name="inputGroup1"
+                    placeholder="Enter number of employees.."
+                    onChange={handleRange}
+                />
+                 <input type="text" name="inputGroup1Label" className="form-control col-1">{formData.value}</input>
+                <span className="input-group-text bg-primary p-2 col-1">{formData.value}</span> */}
+
+
   return (
     <form className="input-group" onSubmit={handleSubmit}>
         <div className="input-group m-1 mt-0">
@@ -59,35 +69,32 @@ function SearchForm({ searchFunction, term }) {
         </div>
         <div className="input-group">
             <div className="input-group row container-fluid m-1 p-0 flex-nowrap">
-                <label className="input-group-text bg-secondary p-2 col-3" htmlFor="inputGroup1">Min Employees</label>
-                <input type="range" className="form-range form-control p-2 col-9" min="0" max="500000" step = "500"
+                <label className="input-group-text bg-secondary p-2 col-4 col-sm-4 col-md-3 col-lg-2 col-xl-2 col-xxl-2" htmlFor="inputGroup1">Min Employees</label>
+                <input className="form-control col-7 col-sm-7 col-md-9 col-lg-10 col-xl-10 col-xxl-10"
                     name="inputGroup1"
                     placeholder="Enter number of employees.."
                     onChange={handleRange}
                 />
-                 <input type="text" name="inputGroup1Label" className="form-control col-1">{formData.value}</input>
-                <span className="input-group-text bg-primary p-2 col-1">{formData.value}</span>
-
             </div>
             <div className="input-group row container-fluid m-1 p-0 flex-nowrap">
-                <label className="input-group-text bg-secondary p-2 col-3" htmlFor="inputGroup2">Max Employees</label>
-                <input className="form-control col-3"
+                <label className="input-group-text bg-secondary p-2 col-4 col-sm-4 col-md-3 col-lg-2 col-xl-2 col-xxl-2" htmlFor="inputGroup2">Max Employees</label>
+                <input className="form-control col-7 col-sm-7 col-md-9 col-lg-10 col-xl-10 col-xxl-10"
                     id="inputGroup2"
                     placeholder="Enter number of employees.."
                     onChange={handleChange}
                 />
             </div>
             <div className="input-group row container-fluid m-1 p-0  flex-nowrap">
-                <label className="input-group-text bg-secondary p-2 col-3" htmlFor="inputGroup3">Minimum Salary</label>
-                <input className="form-control col-9"
+                <label className="input-group-text bg-secondary p-2 col-4 col-sm-4 col-md-3 col-lg-2 col-xl-2 col-xxl-2" htmlFor="inputGroup3">Minimum Salary</label>
+                <input className="form-control col-7 col-sm-7 col-md-9 col-lg-10 col-xl-10 col-xxl-10"
                     id="inputGroup3"
                     placeholder="Enter salary number.."
                     onChange={handleChange}
                 />
             </div>
             <div className="input-group row container-fluid m-1 p-0  flex-nowrap">
-                <label className="input-group-text bg-secondary p-2 col-3" htmlFor="inputGroup4" defaultValue="Choose..">Offers Equity</label>
-                <select className="form-select col-9"
+                <label className="input-group-text bg-secondary p-2 col-4 col-sm-4 col-md-3 col-lg-2 col-xl-2 col-xxl-2" htmlFor="inputGroup4" defaultValue="Choose..">Offers Equity</label>
+                <select className="form-select col-7 col-sm-7 col-md-9 col-lg-10 col-xl-10 col-xxl-10"
                     id="inputGroup4"
                     onChange={handleChange}>
                     <option>Choose..</option>
